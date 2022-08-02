fe12e4c73bb8908e422a4fac468fed4ec76d07a2
chin <a5566qq2581@gmail.com>
2022-08-02 23:02:02 +0800

    feat: add infernce function to direct call
    - Use function to get the infernce output, next
    that can be used for web API or commander

---
+++ inference_cpu.py
@@ -0,0 +1,192 @@
+# modify from denseNet_localization.py
+# input single DICOM file and output multiple GSPS label DICOM files in input directory
+
+import numpy as np
+import torch
+from torch.utils.data import Dataset
+from torch.nn import functional as F
+import torch.nn as nn
+
+import torchvision.transforms as transforms
+from torch.autograd import Variable
+
+import cv2
+import sys
+import os
+
+from collections import OrderedDict
+import scipy.ndimage as ndimage
+import scipy.ndimage.filters as filters
+from scipy.ndimage import binary_dilation
+
+import local_utils
+from local_utils import DenseNet121, ChestXrayDataSetPlot, GradCAM
+import pydicom
+
+torch.device('cpu')
+is_dcm = False
+ds = None
+
+
+def inference(i_filename: str):
+    """Get inference output
+    Args:
+        i_filename: The full path of filename
+    """
+    global ds
+    img_folder_path, filename = os.path.split(i_filename)
+    test_x = []
+    extension_name = os.path.splitext(i_filename)[1]
+    if extension_name == ".dcm":
+        ds = pydicom.dcmread(i_filename, stop_before_pixels=True)
+    pass
+    loaded_image_np = local_utils.load_and_transform_image(i_filename, ds=ds)
+    test_x.append(loaded_image_np)
+    test_x = np.array(test_x)
+
+    model = DenseNet121(8)
+    model = torch.nn.DataParallel(model)
+    state_dict = torch.load("model/DenseNet121_aug4_pretrain_WeightBelow1_1_0.829766922537.pkl")
+    print("model loaded")
+
+    dataset_transforms = transforms.Compose([
+        transforms.ToPILImage(),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    test_dataset = ChestXrayDataSetPlot(input_x=test_x, transform=dataset_transforms)
+
+    thresholds = np.load("thresholds.npy")
+    print("activate threshold", thresholds)
+
+    print("generate heatmap ..........")
+    heatmap_output, image_id, output_class = create_heatmap(model, test_dataset, thresholds)
+    prediction_dict = plot_bounding_box(image_id, output_class, heatmap_output)
+    for index, p in enumerate(prediction_dict[0]):
+        local_utils.create_gsps_from_bounding_box(ds, p, index + 1, img_folder_path)
+    pass
+
+pass
+
+
+def create_heatmap(model: nn.Module, test_dataset: Dataset, thresholds):
+    # ======== Create heatmap ===========
+
+    heatmap_output = []
+    image_id = []
+    output_class = []
+
+    gcam = GradCAM(model=model, cuda=False)
+    for index in range(len(test_dataset)):
+        input_img = Variable((test_dataset[index]).unsqueeze(0), requires_grad=True)
+        probs = gcam.forward(input_img)
+
+        activate_classes = np.where((probs > thresholds)[0] == True)[0]  # get the activated class
+        for activate_class in activate_classes:
+            gcam.backward(idx=activate_class)
+            output = gcam.generate(target_layer="module.densenet121.features.denseblock4.denselayer16.conv2")
+            #### this output is heatmap ####
+            if np.sum(np.isnan(output)) > 0:
+                print("fxxx nan")
+            heatmap_output.append(output)
+            image_id.append(index)
+            output_class.append(activate_class)
+        print("test ", str(index), " finished")
+
+    print("heatmap output done")
+    print("total number of heatmap: ", len(heatmap_output))
+    return heatmap_output, image_id, output_class
+
+
+pass
+
+
+def plot_bounding_box(image_id, output_class, heatmap_output):
+    # ======= Plot bounding box =========
+    img_width, img_height = 224, 224
+    img_width_exp, img_height_exp = 1024, 1024
+
+    crop_del = 16
+    rescale_factor = 4
+
+    class_index = ['Atelectasis', 'Cardiomegaly', 'Effusion', 'Infiltration', 'Mass', 'Nodule', 'Pneumonia',
+                   'Pneumothorax']
+    avg_size = np.array([[411.8, 512.5, 219.0, 139.1], [348.5, 392.3, 479.8, 381.1],
+                         [396.5, 415.8, 221.6, 318.0], [394.5, 389.1, 294.0, 297.4],
+                         [434.3, 366.7, 168.7, 189.8], [502.4, 458.7, 71.9, 70.4],
+                         [378.7, 416.7, 276.5, 304.5], [369.3, 209.4, 198.9, 246.0]])
+
+    prediction_dict = {0: []}
+    for img_id, k, npy in zip(image_id, output_class, heatmap_output):
+
+        data = npy
+
+        # output avgerge
+        prediction_sent = '%s %.1f %.1f %.1f %.1f' % (
+            class_index[k], avg_size[k][0], avg_size[k][1], avg_size[k][2], avg_size[k][3])
+        prediction_dict[img_id].append(prediction_sent)
+
+        if np.isnan(data).any():
+            continue
+
+        w_k, h_k = (avg_size[k][2:4] * (256 / 1024)).astype(np.int64)
+
+        # Find local maxima
+        neighborhood_size = 100
+        threshold = .1
+
+        data_max = filters.maximum_filter(data, neighborhood_size)
+        maxima = (data == data_max)
+        data_min = filters.minimum_filter(data, neighborhood_size)
+        diff = ((data_max - data_min) > threshold)
+        maxima[diff == 0] = 0
+        for _ in range(5):
+            maxima = binary_dilation(maxima)
+
+        labeled, num_objects = ndimage.label(maxima)
+        slices = ndimage.find_objects(labeled)
+        xy = np.array(ndimage.center_of_mass(data, labeled, range(1, num_objects + 1)))
+
+        for pt in xy:
+            if data[int(pt[0]), int(pt[1])] > np.max(data) * .9:
+                upper = int(max(pt[0] - (h_k / 2), 0.))
+                left = int(max(pt[1] - (w_k / 2), 0.))
+
+                right = int(min(left + w_k, img_width))
+                lower = int(min(upper + h_k, img_height))
+
+                prediction_sent = '%s %.1f %.1f %.1f %.1f' % (class_index[k], (left + crop_del) * rescale_factor,
+                                                              (upper + crop_del) * rescale_factor,
+                                                              (right - left) * rescale_factor,
+                                                              (lower - upper) * rescale_factor)
+
+                prediction_dict[img_id].append(prediction_sent)
+    return prediction_dict
+
+
+pass
+
+
+def visualize_prediction_image(p, image_path):
+    frame = cv2.imread(image_path)
+    prediction_name, temp_x1, temp_y1, temp_x2, temp_y2 = p.split(" ")
+    if temp_x1 < temp_x2:
+        x1, y1, x2, y2 = [np.round(float(temp_x1)),
+                          np.round(float(temp_y1)),
+                          np.round(float(temp_x2)),
+                          np.round(float(temp_y2))]
+
+    else:
+        x1, y1, x2, y2 = [np.round(float(temp_x2)),
+                          np.round(float(temp_y2)),
+                          np.round(float(temp_x1)),
+                          np.round(float(temp_y1))]
+    cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 255, 0), 3)  # rgb 220,20,60
+    cv2.imshow('image', frame)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+pass
+
--- local_utils.py
+++ local_utils.py
@@ -9,9 +9,15 @@ import pydicom
 import scipy
 import skimage
 import skimage.transform
+import torch
+from torch.nn import functional as F
 import torch.nn as nn
+from torch.autograd import Variable
 import torchvision
+from torch.utils.data import Dataset
 from pydicom.valuerep import PersonName
+from collections import OrderedDict
+
 
 
 # model archi
@@ -37,6 +43,143 @@ class DenseNet121(nn.Module, ABC):
         x = self.densenet121(x)
         return x
 
+    pass
+
+
+pass
+
+
+# build test dataset
+class ChestXrayDataSetPlot(Dataset):
+    def __init__(self, input_x, transform=None):
+        self.X = np.uint8(input_x * 255)
+        self.transform = transform
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index: the index of item
+        Returns:
+            image
+        """
+        current_X = np.tile(self.X[index], 3)
+        image = self.transform(current_X)
+        return image
+    pass
+
+    def __len__(self):
+        return len(self.X)
+    pass
+
+
+pass
+
+
+# ======= Grad CAM Function =========
+class PropagationBase(object):
+
+    def __init__(self, model, cuda=False):
+        self.model = model
+        self.model.eval()
+        if cuda:
+            self.model.cuda()
+        self.cuda = cuda
+        self.all_fmaps = OrderedDict()
+        self.all_grads = OrderedDict()
+        self._set_hook_func()
+        self.image = None
+
+    def _set_hook_func(self):
+        raise NotImplementedError
+
+    def _encode_one_hot(self, idx):
+        one_hot = torch.FloatTensor(1, self.preds.size()[-1]).zero_()
+        one_hot[0][idx] = 1.0
+        return one_hot.cuda() if self.cuda else one_hot
+
+    def forward(self, image):
+        self.image = image
+        self.preds = self.model.forward(self.image)
+        return self.preds.cpu().data.numpy()
+
+    def backward(self, idx):
+        self.model.zero_grad()
+        one_hot = self._encode_one_hot(idx)
+        if self.cuda:
+            self.preds.backward(gradient=one_hot, retain_graph=True)
+        else:
+            self.preds.cpu().backward(gradient=one_hot, retain_graph=True)
+        pass
+
+
+pass
+
+
+class GradCAM(PropagationBase):
+
+    def _set_hook_func(self):
+
+        def func_f(module, input, output):
+            self.all_fmaps[id(module)] = output.data.cpu()
+        pass
+
+        def func_b(module, grad_in, grad_out):
+            self.all_grads[id(module)] = grad_out[0].cpu()
+        pass
+
+        for module in self.model.named_modules():
+            module[1].register_forward_hook(func_f)
+            module[1].register_backward_hook(func_b)
+        pass
+    pass
+
+    def _find(self, outputs, target_layer):
+        for key, value in outputs.items():
+            for module in self.model.named_modules():
+                if id(module[1]) == key:
+                    if module[0] == target_layer:
+                        return value
+                    pass
+                pass
+            pass
+        pass
+        raise ValueError('Invalid layer name: {}'.format(target_layer))
+    pass
+
+    def _normalize(self, grads):
+        l2_norm = torch.sqrt(torch.mean(torch.pow(grads, 2))) + 1e-5
+        return grads / l2_norm.data
+    pass
+
+    def _compute_grad_weights(self, grads):
+        grads = self._normalize(grads)
+        self.map_size = grads.size()[2:]
+        return nn.AvgPool2d(self.map_size)(grads)
+    pass
+
+    def generate(self, target_layer):
+        fmaps = self._find(self.all_fmaps, target_layer)
+        grads = self._find(self.all_grads, target_layer)
+        weights = self._compute_grad_weights(grads)
+        gcam = torch.FloatTensor(self.map_size).zero_()
+        for fmap, weight in zip(fmaps[0], weights[0]):
+            gcam += fmap * weight.data
+
+        gcam = F.relu(Variable(gcam))
+
+        gcam = gcam.data.cpu().numpy()
+        gcam -= gcam.min()
+        gcam /= gcam.max()
+        gcam = cv2.resize(gcam, (self.image.size(3), self.image.size(2)))
+
+        return gcam
+    pass
+
+    def save(self, filename, gcam, raw_image):
+        gcam = cv2.applyColorMap(np.uint8(gcam * 255.0), cv2.COLORMAP_JET)
+        gcam = gcam.astype(np.float) + raw_image.astype(np.float)
+        gcam = gcam / gcam.max() * 255.0
+        cv2.imwrite(filename, np.uint8(gcam))
     pass
 
 
